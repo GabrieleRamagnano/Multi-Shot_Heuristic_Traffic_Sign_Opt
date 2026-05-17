8ec6f9a0883f7e3f904e4b60b072de9ab9f22004
GabrieleRamagnano <119187111+GabrieleRamagnano@users.noreply.github.com>
2026-05-17 12:48:41 +0200

    .gitignore updated

--- heuristic/experiments/heu_105/inst4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-echo "--------------------------------------------"
-bash ./inst5.sh hphase hlink OPT_dhxphase OPT_dhxplink OPT_clingcon heuristic_nodelta hphase hlink
-echo "--------------------------------------------"
-bash ./inst5.sh "hphase(-theory)" "hlink(-theory)" OPT_dhxphase_plus OPT_dhxphase OPT_clingcon heuristic_original hphase hlink
-echo "--------------------------------------------"
