a7b1966475a3345a6d2c24c388bc1f4f0bb8dcdb
GabrieleRamagnano <119187111+GabrieleRamagnano@users.noreply.github.com>
2026-05-17 12:26:37 +0200

    .gitignore updated

--- heuristic/experiments/theory_delta/see0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -r results.txt
-bash ./compare0.sh opdelta OPT_aggrel1
-echo "--------------------------------------------"
-bash ./compare0.sh bndelta det_bound_NotOpt_aggregatel1
-echo "--------------------------------------------"
-bash ./compare0.sh opdeltime OPT_deltime
